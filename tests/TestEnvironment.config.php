a23aa031d01ac3599d6fd4fdbe2ec9822bf3934c
Matthias Molitor <matthias@matthimatiker.de>
2012-06-12 23:08:37 +0200

    added bootstrap files

---
+++ tests/TestEnvironment.config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Initializes the test environment.
+ *
+ * @category PHP
+ * @package MolComponents
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 12.06.2012
+ */
+
+/** Initialize autoloader. */
+require_once(dirname(__FILE__) . '/../vendor/autoload.php');
