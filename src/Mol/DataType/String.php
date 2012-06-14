d55f80ec40e7eb50f68163369ad6ef1c37fc4a3c
Matthias Molitor <matthias@matthimatiker.de>
2012-06-14 21:44:36 +0200

    added string class stub

---
+++ src/Mol/DataType/String.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_DataType_String
+ *
+ * @category PHP
+ * @package Mol_DataType
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 14.06.2012
+ */
+
+/**
+ * Class that simplifies string handling.
+ *
+ * @category PHP
+ * @package Mol_DataType
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 14.06.2012
+ */
+class Mol_DataType_String
+{
+    
+}
