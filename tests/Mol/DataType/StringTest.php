852d977ad60b05bdcb01b3bc37eb2ef7277b0f76
Matthias Molitor <matthias@matthimatiker.de>
2012-06-16 15:00:06 +0200

    added test case stub

---
+++ tests/Mol/DataType/StringTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_DataType_StringTest
+ *
+ * @category PHP
+ * @package Mol_DataType
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 16.06.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the String class.
+ *
+ * @category PHP
+ * @package Mol_DataType
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 16.06.2012
+ */
+class Mol_DataType_StringTest extends PHPUnit_Framework_TestCase
+{
+    
+}
