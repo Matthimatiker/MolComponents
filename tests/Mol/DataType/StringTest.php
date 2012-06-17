a7758875fab0122f962b0fe1450ffb6fee9b3eff
Matthias Molitor <matthias@matthimatiker.de>
2012-06-17 16:15:09 +0200

    added method doc blocks

--- tests/Mol/DataType/StringTest.php
+++ tests/Mol/DataType/StringTest.php
@@ -319,221 +319,369 @@ class Mol_DataType_StringTest extends PHPUnit_Framework_TestCase
         
     }
     
+    /**
+     * Checks if subString() extracts the correct part of the string.
+     */
     public function testSubStringExtractRequestedPartOfString()
     {
         
     }
     
+    /**
+     * Ensures that the stubString() is extended to end of the original string if no
+     * length parameter is provided.
+     */
     public function testSubStringExtendsSubStringToEndOfOriginalStringIfLengthIsNotProvided()
     {
         
     }
     
+    /**
+     * Ensures that the stubString() is extended to end of the original string if the provided
+     * length exceeds the length of the original string.
+     */
     public function testSubStringExtendsSubStringToEndOfOriginalStringIfLengthExceedsOriginalString()
     {
         
     }
     
+    /**
+     * Checks if subString() handles multi-byte characters (for example umlauts) correctly.
+     */
+    public function testSubStringWorksWithUmlauts()
+    {
+        
+    }
+    
+    /**
+     * Checks if toUpperCase() returns the correct string.
+     */
     public function testToUpperCaseReturnsCorrectValue()
     {
         
     }
     
+    /**
+     * Checks if toUpperCase() treats umlauts correctly.
+     */
     public function testToUpperCaseWorksWithUmlauts()
     {
         
     }
     
+    /**
+     * Checks if toLowerCase() returns the correct string.
+     */
     public function testToLowerCaseReturnsCorrectValue()
     {
         
     }
     
+    /**
+     * Checks if toLowerCase() treats umlauts correctly.
+     */
     public function testToLowerCaseWorksWithUmlauts()
     {
         
     }
     
+    /**
+     * Checks if trim() removes whitespace from the start of the string.
+     */
     public function testTrimRemovesWhitespaceFromStart()
     {
         
     }
     
+    /**
+     * Checks if trim() removes whitespace from the end of the string.
+     */
     public function testTrimRemovesWhitespaceFromEnd()
     {
         
     }
     
+    /**
+     * Checks if trim() removes the provided characters from the start of the string.
+     */
     public function testTrimRemovesProvidedCharactersFromStart()
     {
         
     }
     
+    /**
+     * Checks if trim() removes the provided characters from the end of the string.
+     */
     public function testTrimRemovesProvidedCharactersFromEnd()
     {
         
     }
     
+    /**
+     * Checks if trimLeft() removes whitespace from the start of the string.
+     */
     public function testTrimLeftRemovesWhitespaceFromStart()
     {
         
     }
     
+    /**
+     * Ensures that trimLeft() does not touch whitespace at the end of the string.
+     */
     public function testTrimLeftDoesNotTouchWhitespaceAtTheEndOfTheString()
     {
         
     }
     
+    /**
+     * Checks if trimLeft() removes the provided characters from the start of the string.
+     */
     public function testTrimLeftRemovesProvidedCharactersFromStart()
     {
     
     }
     
+    /**
+     * Ensures that trimLeft() does not touch the characters at the end of the string.
+     */
     public function testTrimLeftDoesNotTouchProvidedCharactersAtTheEndOfTheString()
     {
     
     }
     
+    /**
+     * Checks if trimRight() removes whitespace from the end of the string.
+     */
     public function testTrimRightRemovesWhitespaceFromEnd()
     {
     
     }
     
+    /**
+     * Ensures that trimRight() does not touch whitespace at the start of the string.
+     */
     public function testTrimRightDoesNotTouchWhitespaceAtTheStartOfTheString()
     {
     
     }
     
+    /**
+     * Checks if trimRight() removes the provided characters from the end of the string.
+     */
     public function testTrimRightRemovesProvidedCharactersFromEnd()
     {
     
     }
     
+    /**
+     * Ensures that trimRight() does not touch characters at the start of the string.
+     */
     public function testTrimRightDoesNotTouchProvidedCharactersAtTheStartOfTheString()
     {
     
     }
     
+    /**
+     * Checks if toCharacters() returns an array.
+     */
     public function testToCharactersReturnsArray()
     {
         
     }
     
+    /**
+     * Checks if toCharacters() returns the expected number of characters.
+     */
     public function testToCharactersReturnsExpectedNumberOfCharacters()
     {
         
     }
     
+    /**
+     * Ensures that toCharacters() returns the correct characters.
+     */
     public function testToCharactersReturnsCorrectCharacters()
     {
         
     }
     
+    /**
+     * Checks if toCharacters() returns the characters in correct order.
+     */
     public function testToCharactersReturnsCharactersInCorrectOrder()
     {
         
     }
     
+    /**
+     * Checks if toCharacters() handles multi-byte characters (for example
+     * umlauts) correctly.
+     */
     public function testToCharactersWorksWithUmlauts()
     {
         
     }
     
+    /**
+     * Checks if the string object is traverable.
+     */
     public function testStringIsTraversable()
     {
         
     }
     
+    /**
+     * Ensures that getIterator() returns an instance of Traversable.
+     */
     public function testGetIteratorReturnsTraversable()
     {
         
     }
     
+    /**
+     * Checks if the iteration loops through the characters of the string.
+     */
     public function testIterationLoopsThroughCharacters()
     {
         
     }
     
+    /**
+     * Ensures that equals() returns true if the string are equal.
+     */
     public function testEqualsReturnsTrueIfStringsAreEqual()
     {
         
     }
     
+    /**
+     * Ensures that equals() returns false if the compared strings
+     * have different lengths.
+     */
     public function testEqualsReturnsFalseIfStringLengthIsNotEqual()
     {
         
     }
     
+    /**
+     * Ensures that equals() returns false if teh compared string have the
+     * same length, but their content differs.
+     */
     public function testEqualsReturnsFalseIfStringContentDiffers()
     {
         
     }
     
+    /**
+     * Checks if length() returns an integer.
+     */
     public function testLengthReturnsInteger()
     {
         
     }
     
+    /**
+     * Checks if length() returns the number of characters.
+     */
     public function testLengthReturnsCorrectValue()
     {
         
     }
     
+    /**
+     * Ensures that length() returns the correct number of characters even
+     * if the string contains multi-byte characters.
+     */
     public function testLengthReturnsCorrectValueIfStringContainsUmlauts()
     {
         
     }
     
+    /**
+     * Ensures that lengthInBytes() returns an integer.
+     */
     public function testLengthInBytesReturnsInteger()
     {
         
     }
     
+    /**
+     * Checks if lengthInBytes() returns the correct number of bytes.
+     */
     public function testLengthInBytesReturnsCorrectValue()
     {
         
     }
     
+    /**
+     * Ensures that lengthInBytes() returns the correct value if the string
+     * contains multi-byte characters.
+     */
     public function testLengthInBytesReturnsCorrectValueIfStringContainsUmlauts()
     {
         
     }
     
+    /**
+     * Checks if the string is countable.
+     */
     public function testStringIsCountable()
     {
         
     }
     
+    /**
+     * Ensures that count() returns the same value as length().
+     */
     public function testCountReturnsSameValueAsLength()
     {
         
     }
     
+    /**
+     * Ensures that isEmpty() returns true if the length of the string is 0.
+     */
     public function testIsEmptyReturnsTrueIfStringLengthIsZero()
     {
         
     }
     
+    /**
+     * Ensures that isEmpty() returns true if the string contains only whitespace.
+     */
     public function testIsEmptyReturnsTrueIfStringContainsOnlyWhitespace()
     {
         
     }
     
+    /**
+     * Ensures that isEmpty() returns false if the string contains non-whitespace
+     * characters.
+     */
     public function testIsEmptyReturnsFalseIfStringContainsNonWhitespaceCharacters()
     {
         
     }
     
+    /**
+     * Checks if toString() returns a string.
+     */
     public function testToStringReturnsString()
     {
         
     }
     
+    /**
+     * Checks if toString() returns the correct string.
+     */
     public function testToStringReturnsCorrectValue()
     {
         
     }
     
+    /**
+     * Checks if it is possible to cast the object to a string.
+     */
     public function testCastingObjectToStringReturnsCorrectValue()
     {
         
