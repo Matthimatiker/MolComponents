69e1750889e81cd43c5de3b5856bc4337aaf79be
Matthias Molitor <matthias@matthimatiker.de>
2012-06-21 23:22:12 +0200

    added method signatures

--- src/Mol/Util/String.php
+++ src/Mol/Util/String.php
@@ -26,4 +26,108 @@
 class Mol_Util_String
 {
     
+    /**
+     * Checks if the string starts with the provided prefix.
+     *
+     * @param string $subject The tested string.
+     * @param string $prefix
+     * @return boolean True if the string starts with the prefix, false otherwise.
+     */
+    public static function startsWith($subject, $prefix)
+    {
+        
+    }
+    
+    /**
+     * Checks if the string ends with the provided suffix.
+     *
+     * @param string $subject The tested string.
+     * @param string $suffix
+     * @return boolean True if the string ends with the suffix, false otherwise.
+     */
+    public static function endsWith($subject, $suffix)
+    {
+        
+    }
+    
+    /**
+     * Checks if the string contains the provided needle.
+     *
+     * @param string $subject The tested string.
+     * @param string $needle
+     * @return boolean True if the string contains the needle, false otherwise.
+     */
+    public static function contains($subject, $needle)
+    {
+        
+    }
+    
+    /**
+     * Checks if the string contains any of the provided needles.
+     *
+     * @param string $subject The tested string.
+     * @param array(string) $needles
+     * @return boolean True if the string contains a needle, false otherwise.
+     */
+    public static function containsAny($subject, array $needles)
+    {
+    }
+    
+    /**
+     * Checks if the string contains all of the provided needles.
+     *
+     * @param string $subject The tested string.
+     * @param array(string) $needles
+     * @return boolean True if the string contains all needles, false otherwise.
+     */
+    public static function containsAll($subject, array $needles)
+    {
+        
+    }
+    
+    /**
+     * Replaces all occurrences of $searchOrMapping by $replace.
+     *
+     * This method provides 3 signatures:
+     *
+     * replace(string, string, string):
+     * <code>
+     * $result = Mol_Util_String::replace('my string', 'search', 'replace');
+     * </code>
+     * Replaces all occurrences of "search" by "replace".
+     *
+     * replace(string, array(string), string):
+     * <code>
+     * $needles = array(
+     *     'first',
+     *     'seconds'
+     * );
+     * $result = Mol_Util_String::replace('my string', $needles, 'replace');
+     * </code>
+     * Replaces all string that are contained in the $needles array by "replace".
+     *
+     * replace(string, array(string=>string)):
+     * <code>
+     * $mapping = array(
+     *     'first' => 'last',
+     *     'hello' => 'world'
+     * );
+     * $result = Mol_Util_String::replace('my string', $mapping);
+     * </code>
+     * Expects an associative array that represents a mapping of strings
+     * as argument.
+     * The keys are replaced by the assigned values.
+     * In this example occurences of "first" are replaced by "last" and
+     * "hello" is replaced by "world".
+     *
+     * @param string $subject
+     * @param string|array(integer|string=>string) $searchOrMapping
+     * @param string $replace
+     * @return string The string with applied replacements.
+     */
+    public static function replace($subject, $searchOrMapping, $replace = null)
+    {
+        
+    }
+    
 }
