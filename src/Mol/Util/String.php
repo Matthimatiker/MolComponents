0bd4a1bdbf4973fd9a4e62cbfb31abc25fe7fe8e
Matthias Molitor <matthias@matthimatiker.de>
2012-06-21 23:10:56 +0200

    added class stub for string helper

---
+++ src/Mol/Util/String.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Util_String
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 21.06.2012
+ */
+
+/**
+ * Contains helper methods for string handling.
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 21.06.2012
+ */
+class Mol_Util_String
+{
+    
+}
