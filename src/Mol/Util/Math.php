55b62389c907254bd2107f6f9a1fcf423a1d6c5a
Matthias Molitor <matthias@matthimatiker.de>
2012-06-22 22:19:13 +0200

    added helper class stub

---
+++ src/Mol/Util/Math.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Mol_Util_Math
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 22.06.2012
+ */
+
+/**
+ * Provides mathematical helper methods.
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 22.06.2012
+ */
+class Mol_Util_Math
+{
+    
+    public static function sign($value)
+    {
+        
+    }
+    
+    public static function isEven($value)
+    {
+        
+    }
+    
+    public static function isOdd()
+    {
+        
+    }
+    
+}
