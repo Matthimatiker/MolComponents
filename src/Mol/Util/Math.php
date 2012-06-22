833aa9e682b9c686165d5d819eb007a5575757a9
Matthias Molitor <matthias@matthimatiker.de>
2012-06-22 22:30:09 +0200

    added comments

--- src/Mol/Util/Math.php
+++ src/Mol/Util/Math.php
@@ -26,17 +26,56 @@
 class Mol_Util_Math
 {
     
+    /**
+     * Returns the sign of the value.
+     *
+     * This method returns:
+     * # -1 if $value < 0
+     * #  0 if $value = 0
+     * #  1 if $value > 0
+     *
+     * @param integer|double $value
+     * @return integer -1 if $value < 0, 0 if $value == 0 and 1 if $value > 0.
+     */
     public static function sign($value)
     {
         
     }
     
+    /**
+     * Checks if the provided value is even.
+     *
+     * Example:
+     * <code>
+     * // Returns true:
+     * Mol_Util_Math::isEven(2);
+     * // Returns false:
+     * Mol_Util_Math::isEven(7);
+     * </code>
+     *
+     * @param integer $value
+     * @return boolean True if $value is even, false otherwise.
+     */
     public static function isEven($value)
     {
         
     }
     
-    public static function isOdd()
+    /**
+     * Checks if the provided value is odd.
+     *
+     * Example:
+     * <code>
+     * // Returns true:
+     * Mol_Util_Math::isOdd(3);
+     * // Returns false:
+     * Mol_Util_Math::isOdd(6);
+     * </code>
+     *
+     * @param integer $value
+     * @return boolean True if $value is odd, false otherwise.
+     */
+    public static function isOdd($value)
     {
         
     }
