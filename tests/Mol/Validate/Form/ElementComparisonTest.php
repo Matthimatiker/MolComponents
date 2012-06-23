697df6fec17296ad13ca2b873e7a329fb3b7b7bd
Matthias Molitor <matthias@matthimatiker.de>
2012-06-23 12:58:07 +0200

    added test case stub

---
+++ tests/Mol/Validate/Form/ElementComparisonTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Validate_Form_ElementComparisonTest
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 23.06.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the ElementComparison validator.
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @subpackage Test
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 23.06.2012
+ */
+class Mol_Validate_Form_ElementComparisonTest extends PHPUnit_Framework_TestCase
+{
+    
+}
