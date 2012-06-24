6797b974a8c8b81067881404dc15c965a43b0485
Matthias Molitor <matthias@matthimatiker.de>
2012-06-24 14:36:14 +0200

    added Equal validator class stub

---
+++ src/Mol/Validate/Form/Relation/Equal.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Mol_Validate_Form_Relation_Equal
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 24.06.2012
+ */
+
+/**
+ * Validator that checks if two values are equal.
+ *
+ * Can be used in combination with Mol_Validate_Form_ElementRelation.
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 24.06.2012
+ */
+class Mol_Validate_Form_Relation_Equal
+{
+    
+}
