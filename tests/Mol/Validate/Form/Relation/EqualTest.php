4369f74b896fd974dedbeac7f15a58ed58f36252
Matthias Molitor <matthias@matthimatiker.de>
2012-06-24 15:44:36 +0200

    added test case stub

---
+++ tests/Mol/Validate/Form/Relation/EqualTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Validate_Form_Relation_EqualTest
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 24.06.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the Equal relation validator.
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 24.06.2012
+ */
+class Mol_Validate_Form_Relation_EqualTest extends PHPUnit_Framework_TestCase
+{
+    
+}
