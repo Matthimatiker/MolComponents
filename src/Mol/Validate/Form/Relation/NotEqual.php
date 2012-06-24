f65a23bdc091141d5cd03dee09edbb204eba9737
Matthias Molitor <matthias@matthimatiker.de>
2012-06-24 19:04:13 +0200

    implemented NotEqual relation validator

--- src/Mol/Validate/Form/Relation/NotEqual.php
+++ src/Mol/Validate/Form/Relation/NotEqual.php
@@ -29,6 +29,22 @@ class Mol_Validate_Form_Relation_NotEqual extends Zend_Validate_Abstract
 {
     
     /**
+     * Identifier for failure message if values are equal.
+     *
+     * @var string
+     */
+    const EQUAL = 'relationNotEqualValuesEqual';
+    
+    /**
+     * Failure message templates.
+     *
+     * @var array(string=>string)
+     */
+    protected $_messageTemplates = array(
+        self::EQUAL => "Input must not equal '%compareLabel%'"
+    );
+    
+    /**
      * Checks if the two values are not equal.
      *
      * @param mixed $value
@@ -37,7 +53,11 @@ class Mol_Validate_Form_Relation_NotEqual extends Zend_Validate_Abstract
      */
     public function isValid($value, $other = null)
     {
-        
+        if ($value === $other) {
+            $this->_error(self::EQUAL);
+            return false;
+        }
+        return true;
     }
     
 }
