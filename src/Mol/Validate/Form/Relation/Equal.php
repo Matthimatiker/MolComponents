9766851b811940271d41dd903276e0d004a2a659
Matthias Molitor <matthias@matthimatiker.de>
2012-06-24 18:23:00 +0200

    implemented Equal relation validator

--- src/Mol/Validate/Form/Relation/Equal.php
+++ src/Mol/Validate/Form/Relation/Equal.php
@@ -29,6 +29,22 @@ class Mol_Validate_Form_Relation_Equal extends Zend_Validate_Abstract
 {
     
     /**
+     * Identifier for failure message if values are not equal.
+     *
+     * @var string
+     */
+    const NOT_EQUAL = 'relationEqualNotEqual';
+    
+    /**
+     * Failure message templates.
+     *
+     * @var array(string=>string)
+     */
+    protected $_messageTemplates = array(
+        self::NOT_EQUAL => "Input must be equal to '%compareLabel%'"
+    );
+    
+    /**
      * Checks if the two values are equal.
      *
      * @param mixed $value
@@ -37,7 +53,12 @@ class Mol_Validate_Form_Relation_Equal extends Zend_Validate_Abstract
      */
     public function isValid($value, $other = null)
     {
-        
+        $this->_setValue($value);
+        if ($value !== $other) {
+            $this->_error(self::NOT_EQUAL);
+            return false;
+        }
+        return true;
     }
     
 }
