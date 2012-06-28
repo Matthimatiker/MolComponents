b0b332f2eb4539dfda9ff4c2f3f163c2e59a0f1f
Matthias Molitor <matthias@matthimatiker.de>
2012-06-28 19:58:27 +0200

    extended Zend_Validate_Abstract

--- src/Mol/Validate/Form/Relation/LessThanOrEqual.php
+++ src/Mol/Validate/Form/Relation/LessThanOrEqual.php
@@ -25,7 +25,19 @@
  * @link https://github.com/Matthimatiker/AspectPHP
  * @since 28.06.2012
  */
-class Mol_Validate_Form_Relation_LessThanOrEqual
+class Mol_Validate_Form_Relation_LessThanOrEqual extends Zend_Validate_Abstract
 {
+    
+    /**
+     * Checks if $value is less than or equal to $other.
+     *
+     * @param mixed $value
+     * @param mixed $other The compared value.
+     * @return boolean True if the value is less or equal, false otherwise.
+     */
+    public function isValid($value, $other = null)
+    {
+        
+    }
     
 }
