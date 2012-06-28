8ebdaa6f8bc17680a94b819021b0818133467be0
Matthias Molitor <matthias@matthimatiker.de>
2012-06-28 21:29:19 +0200

    added test case stub

---
+++ tests/Mol/Validate/SuffixTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Validate_SuffixTest
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.06.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the Suffix validator.
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.06.2012
+ */
+class Mol_Validate_SuffixTest extends PHPUnit_Framework_TestCase
+{
+    
+}
