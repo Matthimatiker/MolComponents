5c82d9aaa74855db6159bbf74ddf57d7971f0e4c
Matthias Molitor <matthias@matthimatiker.de>
2012-06-28 19:34:05 +0200

    added stub for GreaterThanOrEqual relation

---
+++ src/Mol/Validate/Form/Relation/GreaterThanOrEqual.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Mol_Validate_Form_Relation_GreaterThanOrEqual
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.06.2012
+ */
+
+/**
+ * Validator that checks if a value is greater than or equal to a compared value.
+ *
+ * Can be used in combination with Mol_Validate_Form_ElementRelation.
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.06.2012
+ */
+class Mol_Validate_Form_Relation_GreaterThanOrEqual extends Zend_Validate_Abstract
+{
+    
+    /**
+     * Checks if $value is greater than $other.
+     *
+     * @param mixed $value
+     * @param mixed $other The compared value.
+     * @return boolean True if the value is greater, false otherwise.
+     */
+    public function isValid($value, $other = null)
+    {
+        
+    }
+    
+}
