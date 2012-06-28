68673e8749d731ad985e81462cd0703ed4568a6c
Matthias Molitor <matthias@matthimatiker.de>
2012-06-28 22:19:40 +0200

    added message templates

--- src/Mol/Validate/Suffix.php
+++ src/Mol/Validate/Suffix.php
@@ -28,6 +28,47 @@ class Mol_Validate_Suffix extends Zend_Validate_Abstract
 {
     
     /**
+     * Identifier for failure message if data of invalid type was passed.
+     *
+     * @var string
+     */
+    const INVALID = 'suffixInvalidType';
+    
+    /**
+     * Identifier for failure message if value does not end with any
+     * of the allowed suffixes.
+     *
+     * @var string
+     */
+    const NO_SUFFIX = 'suffixNoAllowedSuffix';
+    
+    /**
+     * Failure message templates.
+     *
+     * @var array(string=>string)
+     */
+    protected $_messageTemplates = array(
+        self::INVALID   => "Invalid data type provided, expected string",
+        self::NO_SUFFIX => "'%value%' must end with one of the following suffixes: %suffixes%"
+    );
+    
+    /**
+     * Mapping of variables that can be used in failure messages.
+     *
+     * @var array(string=>string)
+     */
+    protected $_messageVariables = array(
+        'suffixes'  => 'suffixes'
+    );
+    
+    /**
+     * A list of accepted suffixes.
+     *
+     * @var array(string)
+     */
+    protected $suffixes = null;
+    
+    /**
      * Creates a validator that accepts the provided suffixes.
      *
      * @param array(string)|string $suffixesOrSuffix List of suffixes or a single suffix.
