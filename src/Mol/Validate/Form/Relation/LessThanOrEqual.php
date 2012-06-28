0c6c516685ea334746bd54a0b2277bcb277dd879
Matthias Molitor <matthias@matthimatiker.de>
2012-06-28 19:57:06 +0200

    added class stub for LessThanOrEqual relation validator

---
+++ src/Mol/Validate/Form/Relation/LessThanOrEqual.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Mol_Validate_Form_Relation_LessThanOrEqual
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.06.2012
+ */
+
+/**
+ * Validator that checks if a value is less than or equal to a compared value.
+ *
+ * Can be used in combination with Mol_Validate_Form_ElementRelation.
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.06.2012
+ */
+class Mol_Validate_Form_Relation_LessThanOrEqual
+{
+    
+}
