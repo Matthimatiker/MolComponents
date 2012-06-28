69c8bd252c25c669f07d714b53e055a50aa3e69d
Matthias Molitor <matthias@matthimatiker.de>
2012-06-28 21:04:08 +0200

    added validator class stub

---
+++ src/Mol/Validate/Suffix.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Mol_Validate_Suffix
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.06.2012
+ */
+
+/**
+ * Validator that checks if a value ends with an accepted suffix.
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 28.06.2012
+ */
+class Mol_Validate_Suffix extends Zend_Validate_Abstract
+{
+    
+    /**
+     * Checks if the value ends with an accepted suffix.
+     *
+     * @param string $value
+     * @return boolean
+     */
+    public function isValid($value)
+    {
+        
+    }
+    
+}
