1cb39c3da0fc6e860ba3202c2626c7eb2e86837e
Matthias Molitor <matthias@matthimatiker.de>
2012-06-29 15:41:16 +0200

    added test case stub

---
+++ tests/Mol/Form/Element/EmailAddressTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Form_Element_EmailAddressTest
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 29.06.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the EmailAddress form element.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 29.06.2012
+ */
+class Mol_Form_Element_EmailAddressTest extends PHPUnit_Framework_TestCase
+{
+    
+}
