7a49700e75235f6eac98188c228945fe141c5d8d
Matthias Molitor <matthias@matthimatiker.de>
2012-06-29 15:30:52 +0200

    added element class stub

---
+++ src/Mol/Form/Element/EmailAddress.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Form_Element_EmailAddress
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 29.06.2012
+ */
+
+/**
+ * Pre-configured form element that accepts email addresses.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 29.06.2012
+ */
+class Mol_Form_Element_EmailAddress extends Zend_Form_Element_Text
+{
+    
+}
