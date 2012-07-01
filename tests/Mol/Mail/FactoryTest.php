22d1a1baac345de9a484594ca2768fff0b7b7d08
Matthias Molitor <matthias@matthimatiker.de>
2012-07-01 18:44:43 +0200

    added test case stub

---
+++ tests/Mol/Mail/FactoryTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Mail_FactoryTest
+ *
+ * @category PHP
+ * @package Mol_Mail
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 01.07.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the mail factory.
+ *
+ * @category PHP
+ * @package Mol_Mail
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 01.07.2012
+ */
+class Mol_Mail_FactoryTest extends PHPUnit_Framework_TestCase
+{
+    
+}
