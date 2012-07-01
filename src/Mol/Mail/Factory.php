5d499a3ef0bfccdc9eb36a883fd2cb76d8c78c7a
Matthias Molitor <matthias@matthimatiker.de>
2012-07-01 16:08:26 +0200

    added mail factory class stub

---
+++ src/Mol/Mail/Factory.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Mail_Factory
+ *
+ * @category PHP
+ * @package Mol_Mail
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 01.07.2012
+ */
+
+/**
+ * Factory that uses template configurations to create pre-filled mail objects.
+ *
+ * @category PHP
+ * @package Mol_Mail
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 01.07.2012
+ */
+class Mol_Mail_Factory
+{
+    
+}
