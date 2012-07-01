a59f51c957532906b4c07e99faf7b13b9d1d81f8
Matthias Molitor <matthias@matthimatiker.de>
2012-07-01 23:24:21 +0200

    added comments

--- tests/Mol/Mail/FactoryTest.php
+++ tests/Mol/Mail/FactoryTest.php
@@ -33,71 +33,116 @@ require_once(dirname(__FILE__) . '/bootstrap.php');
 class Mol_Mail_FactoryTest extends PHPUnit_Framework_TestCase
 {
     
+    /**
+     * Ensures that create() throws an exception if the provided template
+     * does not exist.
+     */
     public function testCreateThrowsExceptionIfTemplateDoesNotExist()
     {
         
     }
     
+    /**
+     * Ensures that create() returns a simple mail object without further
+     * configuration if no template (parameter === null) is provided.
+     */
     public function testCreateReturnsMailObjectsIfNoTemplateIsProvided()
     {
         
     }
     
+    /**
+     * Checks if the factory uses the UTF-8 charset for mails per default.
+     */
     public function testMailCharsetIsUtf8PerDefault()
     {
         
     }
     
+    /**
+     * Checks if the factory sets the configured subject.
+     */
     public function testCreateSetsConfiguredSubject()
     {
         
     }
     
+    /**
+     * Ensures that the configured subject is translated.
+     */
     public function testCreateTranslatesSubject()
     {
         
     }
     
+    /**
+     * Checks if the factory adds the configured to-recpients.
+     */
     public function testCreateAddsConfiguredToRecipients()
     {
         
     }
     
+    /**
+     * Checks if the factory adds the configured cc-recpients.
+     */
     public function testCreateAddsConfiguredCcRecipients()
     {
     
     }
     
+    /**
+     * Checks if the factory adds the configured bcc-recpients.
+     */
     public function testCreateAddsConfiguredBccRecipients()
     {
     
     }
     
+    /**
+     * Checks if the factory sets the configured sender.
+     */
     public function testCreateSetsConfiguredSender()
     {
         
     }
     
+    /**
+     * Ensures that the factory uses the configured charset.
+     */
     public function testCreateSetsConfiguredCharset()
     {
         
     }
     
+    /**
+     * Ensures that create() throws an exception if a configured view script
+     * does not exist.
+     */
     public function testCreateThrowsExceptionIfConfiguredViewScriptIsNotAvailable()
     {
     
     }
     
+    /**
+     * Checks if create() passes the provided params to the view object.
+     */
     public function testCreatePassesParametersToView()
     {
     
     }
     
+    /**
+     * Checks if create() renders the configured view script for the text version.
+     */
     public function testCreateRendersConfiguredTextScript()
     {
         
     }
     
+    /**
+     * Checks if create() renders the configured view script for the HTML version.
+     */
     public function testCreateRendersConfiguredHtmlScript()
     {
         
