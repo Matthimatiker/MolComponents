8f6a04ae3cfe30fbe00069475165861ed5d45d65
Matthias Molitor <matthias@matthimatiker.de>
2012-07-05 19:18:54 +0200

    documented new setting

--- src/Mol/Mail/Factory.php
+++ src/Mol/Mail/Factory.php
@@ -39,6 +39,7 @@
  * # to          (array(string)) - List of default "to" recipients
  * # cc          (array(string)) - List of default "cc" recipients
  * # bcc         (array(string)) - List of default "bcc" recipients
+ * # replyTo     (string)        - A reply-to address
  * # from        (string)        - Sender address
  * # script.text (string)        - Template that renders the text part
  * # script.html (string)        - Template that renders the HTML part
