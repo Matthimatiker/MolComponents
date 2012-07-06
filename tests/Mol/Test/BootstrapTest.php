4de456f734eeebc45007670dd2acacb52d6c2679
Matthias Molitor <matthias@matthimatiker.de>
2012-07-06 15:53:55 +0200

    added test case stub

---
+++ tests/Mol/Test/BootstrapTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Test_BootstrapTest
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 06.07.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the bootstrapper that is used for testing.
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 06.07.2012
+ */
+class Mol_Test_BootstrapTest extends PHPUnit_Framework_TestCase
+{
+    
+}
