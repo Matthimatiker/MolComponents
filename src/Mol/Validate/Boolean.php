7b23e332f1d473214fcd6b77dbd1a634e9dd001e
Matthias Molitor <matthias@matthimatiker.de>
2012-07-12 19:15:52 +0200

    added additional check to avoid type conversion notices if PHP 5.4 is used (#14)

--- src/Mol/Validate/Boolean.php
+++ src/Mol/Validate/Boolean.php
@@ -25,6 +25,7 @@
  */
 class Mol_Validate_Boolean extends Zend_Validate_InArray
 {
+    
     /**
      * Creates the validator.
      */
@@ -52,6 +53,25 @@ class Mol_Validate_Boolean extends Zend_Validate_InArray
         );
         parent::__construct($options);
     }
+    
+    /**
+     * Checks if the provided value represents a boolean.
+     *
+     * @param mixed $value
+     * @return boolean True if $value represents a boolean, false otherwise.
+     */
+    public function isValid($value)
+    {
+        if (is_array($value)) {
+            // Arrays are not accepted.
+            // Ensure that the value is not passed to the parent
+            // isValid() method as it raises an error when generating
+            // the failure message if PHP 5.4 is used.
+            // See #14 for details:
+            // https://github.com/Matthimatiker/MolComponents/issues/14
+            return false;
+        }
+        return parent::isValid($value);
+    }
 
 }
-
