5f12a2aa8db11735379baf4c4007903e763858a2
Matthias Molitor <matthias@matthimatiker.de>
2012-07-14 22:01:18 +0200

    fixed links

--- src/Mol/Application/Bootstrap.php
+++ src/Mol/Application/Bootstrap.php
@@ -8,7 +8,7 @@
  * @author Matthias Molitor <matthias@matthimatiker.de>
  * @copyright 2012 Matthias Molitor
  * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
- * @link https://github.com/Matthimatiker/AspectPHP
+ * @link https://github.com/Matthimatiker/MolComponents
  * @since 14.07.2012
  */
 
@@ -20,7 +20,7 @@
  * @author Matthias Molitor <matthias@matthimatiker.de>
  * @copyright 2012 Matthias Molitor
  * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
- * @link https://github.com/Matthimatiker/AspectPHP
+ * @link https://github.com/Matthimatiker/MolComponents
  * @since 14.07.2012
  */
 class Mol_Application_Bootstrap extends Zend_Application_Bootstrap_Bootstrap
