b160c1de698c2d1a31af5af2b99f9827ee651970
Matthias Molitor <matthias@matthimatiker.de>
2012-07-14 21:56:56 +0200

    added bootstrapper class stub

---
+++ src/Mol/Application/Bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Application_Bootstrap
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 14.07.2012
+ */
+
+/**
+ * Bootstrapper that adds support for lazy loading.
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/AspectPHP
+ * @since 14.07.2012
+ */
+class Mol_Application_Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+    
+}
