80248ec97cd5909bb5c4000792f11d42f8589152
Matthias Molitor <matthias@matthimatiker.de>
2012-07-15 00:25:27 +0200

    added test case stub

---
+++ tests/Mol/Application/BootstrapTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Application_BootstrapTest
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 15.07.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the bootstrapper with lazy loading support.
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 15.07.2012
+ */
+class Mol_Application_BootstrapTest extends PHPUnit_Framework_TestCase
+{
+    
+}
