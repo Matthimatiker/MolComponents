16868e40d16ff2aa930b4b98c44116837c20b587
Matthias Molitor <matthias@matthimatiker.de>
2012-07-15 18:31:43 +0200

    implemented resource interface

--- src/Mol/Application/Bootstrap/LazyLoad/ResourceDecorator.php
+++ src/Mol/Application/Bootstrap/LazyLoad/ResourceDecorator.php
@@ -24,7 +24,73 @@
  * @link https://github.com/Matthimatiker/MolComponents
  * @since 15.07.2012
  */
-class Mol_Application_Bootstrap_LazyLoad_ResourceDecorator
+class Mol_Application_Bootstrap_LazyLoad_ResourceDecorator implements Zend_Application_Resource_Resource
 {
+    /**
+     * Decorates the provided resource.
+     *
+     * @param Zend_Application_Resource_Resource $resource
+     * @throws InvalidArgumentException If no resource was provided.
+     */
+    public function __construct($resource = null)
+    {
+        
+    }
+    
+    /**
+     * Forwards the bootstrapper to the inner resource.
+     *
+     * @param Zend_Application_Bootstrap_Bootstrapper $bootstrap
+     * @return Zend_Application_Resource_Resource Provides a fluent interface.
+     */
+    public function setBootstrap(Zend_Application_Bootstrap_Bootstrapper $bootstrap)
+    {
+        
+    }
+    
+    /**
+     * Retrieves the bootstrapper from the inner resource.
+     *
+     * @return Zend_Application_Bootstrap_Bootstrapper
+     */
+    public function getBootstrap()
+    {
+        
+    }
+    
+    /**
+     * Forwards options to the inner resource.
+     *
+     * @param array(string|integer=>mixed) $options
+     * @return Zend_Application_Resource_Resource Provides a fluent interface.
+     */
+    public function setOptions(array $options)
+    {
+        
+    }
+    
+    /**
+     * Returns the options of the inner resource.
+     *
+     * @return array(string|integer=>mixed)
+     */
+    public function getOptions()
+    {
+        
+    }
+    
+    /**
+     * Creates a lazy loader that is used to initialize the
+     * inner resource later.
+     *
+     * Creates a new loader on each call to honor that the init() method of
+     * the inner resource would usually be called multiple times too.
+     *
+     * @return Mol_Application_Bootstrap_LazyLoader
+     */
+    public function init()
+    {
+        
+    }
     
 }
