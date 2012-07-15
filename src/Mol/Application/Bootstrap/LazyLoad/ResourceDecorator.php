0ed14f13d6d3f535d7d4b44fdac76282f7220328
Matthias Molitor <matthias@matthimatiker.de>
2012-07-15 15:23:46 +0200

    added resource decorator class stub

---
+++ src/Mol/Application/Bootstrap/LazyLoad/ResourceDecorator.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Mol_Bootstrap_LazyLoad_ResourceDecorator
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 15.07.2012
+ */
+
+/**
+ * An application resource decorator that delays initialization by using
+ * a LazyLoader instance.
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 15.07.2012
+ */
+class Mol_Bootstrap_LazyLoad_ResourceDecorator
+{
+    
+}
