22670cd1bb3818515a5665f415d5482c8d0a12bd
Matthias Molitor <matthias@matthimatiker.de>
2012-07-15 12:48:17 +0200

    added class stub for lazy loader

---
+++ src/Mol/Application/Bootstrap/LazyLoader.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Mol_Bootstrap_LazyLoader
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 15.07.2012
+ */
+
+/**
+ * Class that uses a callback to lazy load resources.
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 15.07.2012
+ */
+class Mol_Bootstrap_LazyLoader
+{
+    
+    /**
+     * Creates a lazy loader.
+     *
+     * @param mixed $callback A callback.
+     */
+    public function __construct($callback)
+    {
+        
+    }
+    
+    /**
+     * Uses the callback to load the resource.
+     *
+     * @return mixed
+     */
+    public function load()
+    {
+        
+    }
+    
+}
