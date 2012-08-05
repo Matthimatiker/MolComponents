d2078a94af7b98f271c6a4381ae8827023d4980e
Matthias Molitor <matthias@matthimatiker.de>
2012-08-05 16:29:33 +0200

    added object builder class stub

---
+++ src/Mol/Util/ObjectBuilder.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Mol_Util_ObjectBuilder
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 05.08.2012
+ */
+
+/**
+ * Creates instances of other classes.
+ *
+ * Optionally enforces a provided type constraint *before* creating
+ * an object.
+ * That is especially useful if the object class was provided by configuration
+ * and a common base class or interface is required.
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 05.08.2012
+ */
+class Mol_Util_ObjectBuilder
+{
+    
+}
