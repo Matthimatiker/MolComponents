a8cacbb10d50266a181d15505af8641cd9c6aa28
Matthias Molitor <matthias@matthimatiker.de>
2012-09-02 14:17:41 +0200

    added test case stub

---
+++ tests/Mol/Util/ObjectBuilderTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Util_ObjectBuilderTest
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 02.09.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the object builder.
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 02.09.2012
+ */
+class Mol_Util_ObjectBuilderTest extends PHPUnit_Framework_TestCase
+{
+    
+}
