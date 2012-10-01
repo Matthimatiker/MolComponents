e6f492feca2164eed7b7698b55dc62c53b0cd85d
Matthias Molitor <matthias@matthimatiker.de>
2012-10-01 20:48:09 +0200

    added interface stub for form factory plugins

---
+++ src/Mol/Form/Factory/Plugin.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Form_Factory_Plugin
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.10.2012
+ */
+
+/**
+ * Interface for plugins that can be added to the form factory.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.10.2012
+ */
+interface Mol_Form_Factory_Plugin
+{
+    
+}
