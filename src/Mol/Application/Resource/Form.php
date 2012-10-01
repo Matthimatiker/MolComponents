b7eadff3d78925b46d7d2388648a1a035ad06e9f
Matthias Molitor <matthias@matthimatiker.de>
2012-10-01 20:47:45 +0200

    added class stub for form factory resource

---
+++ src/Mol/Application/Resource/Form.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Mol_Application_Resource_Form
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.10.2012
+ */
+
+/**
+ * Initializes the form factory.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.10.2012
+ */
+class Mol_Application_Resource_Form extends Zend_Application_Resource_ResourceAbstract
+{
+    
+    /**
+     * Creates a pre-configured form factory.
+     *
+     * @return Mol_Form_Factory
+     */
+    public function init()
+    {
+        
+    }
+    
+}
