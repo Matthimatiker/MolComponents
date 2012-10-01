29b48b7c24b4f9435b7d110e151039a6ba0f1478
Matthias Molitor <matthias@matthimatiker.de>
2012-10-01 20:38:06 +0200

    added form factory stub

---
+++ src/Mol/Form/Factory.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Form_Factory
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.10.2012
+ */
+
+/**
+ * Factory that creates form instances.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.10.2012
+ */
+class Mol_Form_Factory
+{
+    
+}
