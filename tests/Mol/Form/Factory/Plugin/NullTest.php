973812951aea35ac42a5f761c1a9db3f5b307e68
Matthias Molitor <matthias@matthimatiker.de>
2012-10-03 14:04:34 +0200

    added plugin test case stub

---
+++ tests/Mol/Form/Factory/Plugin/NullTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Mol_Form_Factory_Plugin_NullTest
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 03.10.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the Null plugin for the form factory.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 03.10.2012
+ */
+class Mol_Form_Factory_Plugin_NullTest extends PHPUnit_Framework_TestCase
+{
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        
+    }
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        
+        parent::tearDown();
+    }
+    
+}
