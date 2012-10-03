97ed165e93767c8d5d6c7686be5d128df9e872ea
Matthias Molitor <matthias@matthimatiker.de>
2012-10-03 13:54:56 +0200

    added plugin class stub

---
+++ src/Mol/Form/Factory/Plugin/Null.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Mol_Form_Factory_Plugin_Null
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 03.10.2012
+ */
+
+/**
+ * Form plugin that does nothing.
+ *
+ * Can be used to disable plugins via configuration or for testing.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 03.10.2012
+ */
+class Mol_Form_Factory_Plugin_Null implements Mol_Form_Factory_Plugin
+{
+    
+    /**
+     * See {@link Mol_Form_Factory_Plugin::enhance()} for details.
+     *
+     * @param Zend_Form $form
+     */
+    public function enhance(Zend_Form $form)
+    {
+        
+    }
+    
+}
