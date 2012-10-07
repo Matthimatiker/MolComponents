4c1cbc26395d83110ed7a07dbb198b9aaad36fa6
Matthias Molitor <matthias@matthimatiker.de>
2012-10-08 00:26:54 +0200

    added plugin class stub

---
+++ src/Mol/Form/Factory/Plugin/CSRF.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Mol_Form_Factory_Plugin_CSRF
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 08.10.2012
+ */
+
+/**
+ * Plugin that adds CSRF tokens to forms.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 08.10.2012
+ */
+class Mol_Form_Factory_Plugin_CSRF implements Mol_Form_Factory_Plugin
+{
+    
+    /**
+     * See {@link Mol_Form_Factory_Plugin::__construct()} for details.
+     *
+     * @param array(string=>mixed) $options
+     */
+    public function __construct(array $options = array())
+    {
+        
+    }
+    
+    /**
+     * Adds a CSRF token to the form if it does not already contain one.
+     *
+     * @param Zend_Form $form
+     */
+    public function enhance(Zend_Form $form)
+    {
+        
+    }
+    
+}
