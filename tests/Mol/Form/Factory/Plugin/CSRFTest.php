8e01fa0783c4d37a8b83adf9333508be247cf026
Matthias Molitor <matthias@matthimatiker.de>
2012-10-08 00:37:33 +0200

    added test case stub

---
+++ tests/Mol/Form/Factory/Plugin/CSRFTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Mol_Form_Factory_Plugin_CSRF
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 08.10.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the CSRF plugin.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 08.10.2012
+ */
+class Mol_Form_Factory_Plugin_CSRFTest extends PHPUnit_Framework_TestCase
+{
+    
+    /**
+     * System under test.
+     *
+     * @var Mol_Form_Factory_Plugin_CSRF
+     */
+    protected $plugin = null;
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->plugin = null;
+    }
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        $this->plugin = null;
+        parent::tearDown();
+    }
+    
+}
