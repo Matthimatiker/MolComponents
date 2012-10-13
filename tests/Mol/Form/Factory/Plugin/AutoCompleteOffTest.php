a782946e018196a87b0984adcc941e8c8e4fd453
Matthias Molitor <matthias@matthimatiker.de>
2012-10-13 18:50:28 +0200

    added test case stub

---
+++ tests/Mol/Form/Factory/Plugin/AutoCompleteOffTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Form_Factory_Plugin_AutoCompleteOffTest
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 13.10.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the AutoCompleteOff plugin.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 13.10.2012
+ */
+class Mol_Form_Factory_Plugin_AutoCompleteOffTest extends PHPUnit_Framework_TestCase
+{
+    
+}
