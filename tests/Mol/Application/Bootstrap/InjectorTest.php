f4452cc1c61159b6476fb942c24c9f9d49b7029a
Matthias Molitor <matthias@matthimatiker.de>
2012-10-14 12:31:02 +0200

    added test cases stub

---
+++ tests/Mol/Application/Bootstrap/InjectorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Application_Bootstrap_InjectorTest
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 14.10.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the bootstrap injector.
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 14.10.2012
+ */
+class Mol_Application_Bootstrap_InjectorTest extends PHPUnit_Framework_TestCase
+{
+    
+}
