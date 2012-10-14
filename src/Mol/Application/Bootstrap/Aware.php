5768fdc2359168793e6924c20b3dbd31057860de
Matthias Molitor <matthias@matthimatiker.de>
2012-10-14 11:42:08 +0200

    added class/interface stubs for bootstrapper injection

---
+++ src/Mol/Application/Bootstrap/Aware.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Mol_Application_Bootstrap_Aware
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 14.10.2012
+ */
+
+/**
+ *
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 14.10.2012
+ */
+interface Mol_Application_Bootstrap_Aware
+{
+    
+    public function setBootstrap(Zend_Application_Bootstrap_BootstrapAbstract $bootstrapper)
+    {
+        
+    }
+    
+}
---
+++ src/Mol/Application/Bootstrap/Injector.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Mol_Application_Bootstrap_Injector
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 14.10.2012
+ */
+
+/**
+ *
+ *
+ * @category PHP
+ * @package Mol_Bootstrap
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 14.10.2012
+ */
+class Mol_Application_Bootstrap_Injector
+{
+    
+    public function __construct(Zend_Application_Bootstrap_BootstrapAbstract $bootstrapper)
+    {
+        
+    }
+    
+    public function inject($object)
+    {
+        
+    }
+    
+}
