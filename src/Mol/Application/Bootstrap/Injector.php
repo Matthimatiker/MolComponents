6fffe45cf355409422b71e11ec28c60275bffe81
Matthias Molitor <matthias@matthimatiker.de>
2012-10-14 12:14:12 +0200

    documented injector

--- src/Mol/Application/Bootstrap/Injector.php
+++ src/Mol/Application/Bootstrap/Injector.php
@@ -13,7 +13,28 @@
  */
 
 /**
+ * Helper class that is able to inject a bootstrapper into
+ * bootstrap aware objects.
  *
+ * = Usage =
+ *
+ * Create an injector:
+ * <code>
+ * $injector = new Mol_Application_Bootstrap_Injector($myBootstrapper);
+ * </code>
+ *
+ * Inject the bootstrapper into an object:
+ * <code>
+ * $injector->inject($object);
+ * </code>
+ * If the given value is not bootstrap aware, then the injector
+ * will just ignore it.
+ *
+ * The injector will also ignore non-objects:
+ * <code>
+ * $value = $injector->inject(null);
+ * </code>
+ * The method inject() returns the provided value afterwards.
  *
  * @category PHP
  * @package Mol_Bootstrap
@@ -26,11 +47,44 @@
 class Mol_Application_Bootstrap_Injector
 {
     
+    /**
+     * Creates an injector.
+     *
+     * Requires the bootstrapper that will be injected as an argument.
+     *
+     * @param Zend_Application_Bootstrap_BootstrapAbstract $bootstrapper
+     */
     public function __construct(Zend_Application_Bootstrap_BootstrapAbstract $bootstrapper)
     {
         
     }
     
+    /**
+     * Tries to inject the bootstrapper into the given object.
+     *
+     * If the given object does not implement Mol_Application_Bootstrap_Aware,
+     * then the injector will do nothing. Otherwise the bootstrapper will be
+     * injected.
+     *
+     * The inject() method always returns the object that was provided as value.
+     *
+     * Examples:
+     * <code>
+     * // Does not try to inject and returns the object:
+     * $object = $injector->inject(new stdClass());
+     *
+     * // Does not try to inject and returns the provided value.
+     * $value = $injector->inject(null);
+     *
+     * // $bootstrapAwareObject implements Mol_Application_Bootstrap_Aware,
+     * // therefore the injector calls setBootstrap() to inject the
+     * // bootstrapper and returns the object.
+     * $object = $injector->inject($bootstrapAwareObject);
+     * </code>
+     *
+     * @param Mol_Application_Bootstrap_Aware|mixed $object
+     * @return Mol_Application_Bootstrap_Aware|mixed The provided object.
+     */
     public function inject($object)
     {
         
