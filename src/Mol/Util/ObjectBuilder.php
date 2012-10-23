84f693d4b9b6ec16a7cbeb571c102712fc08b685
Matthias Molitor <matthias@matthimatiker.de>
2012-10-23 23:50:15 +0200

    changed documentation to show that multiple constraints are possible

--- src/Mol/Util/ObjectBuilder.php
+++ src/Mol/Util/ObjectBuilder.php
@@ -15,10 +15,11 @@
 /**
  * Creates instances of other classes.
  *
- * Optionally enforces a provided type constraint *before* creating
+ * Optionally enforces provided type constraints *before* creating
  * an object.
  * That is especially useful if the object class was provided by configuration
  * and a common base class or interface is required.
+ * It is also possible to require multiple interfaces to be implemented.
  *
  * @category PHP
  * @package Mol_Util
