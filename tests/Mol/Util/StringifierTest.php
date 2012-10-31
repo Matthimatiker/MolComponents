72388296a943758ebb026a09810700f75179a9fe
Matthias Molitor <matthias@matthimatiker.de>
2012-10-31 20:43:05 +0100

    added test case stub

---
+++ tests/Mol/Util/StringifierTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Util_StringifierTest
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 31.10.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the stringifier.
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 31.10.2012
+ */
+class Mol_Util_StringifierTest extends PHPUnit_Framework_TestCase
+{
+    
+}
