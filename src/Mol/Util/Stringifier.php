08da78b51c492fd1bf8cb8aaf98a6230da3cdb79
Matthias Molitor <matthias@matthimatiker.de>
2012-10-31 20:25:17 +0100

    added Stringifier class stub

---
+++ src/Mol/Util/Stringifier.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Mol_Util_Stringifier
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 31.10.2012
+ */
+
+/**
+ * Class that converts data into simple strings.
+ *
+ * This class does *not* serialize data, the generated strings
+ * are just meant for output.
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 31.10.2012
+ */
+class Mol_Util_Stringifier
+{
+    
+}
