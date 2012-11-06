813c9ecddfdc10b97c473dfc75c76f2a32a1201b
Matthias Molitor <matthias@matthimatiker.de>
2012-11-06 23:30:47 +0100

    added class stub

---
+++ src/Mol/Util/TypeInspector.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Mol_Util_TypeInspector
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 06.11.2012
+ */
+
+/**
+ * Helper class that is able to perform type checks.
+ *
+ * This class operates on class and interface names, objects
+ * do not have to be created to perform checks.
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 06.11.2012
+ */
+class Mol_Util_TypeInspector
+{
+    
+}
