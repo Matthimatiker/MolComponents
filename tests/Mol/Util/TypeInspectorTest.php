9b389fa19062ea02fb60bce75d33ba02369025fa
Matthias Molitor <matthias@matthimatiker.de>
2012-11-13 23:56:53 +0100

    added test case stub

---
+++ tests/Mol/Util/TypeInspectorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Util_TypeInspectorTest
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/Mol_Components
+ * @since 13.11.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the type inspector.
+ *
+ * @category PHP
+ * @package Mol_Util
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/Mol_Components
+ * @since 13.11.2012
+ */
+class Mol_Util_TypeInspectorTest extends PHPUnit_Framework_TestCase
+{
+    
+}
