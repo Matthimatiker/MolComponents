0e9805fe1373dfe63edfe78899f57cf00cee6902
Matthias Molitor <matthias@matthimatiker.de>
2012-11-26 20:15:20 +0100

    added comments changed some test names added some tests

--- tests/Mol/Util/TypeInspectorTest.php
+++ tests/Mol/Util/TypeInspectorTest.php
@@ -33,126 +33,231 @@ require_once(dirname(__FILE__) . '/bootstrap.php');
 class Mol_Util_TypeInspectorTest extends PHPUnit_Framework_TestCase
 {
     
+    /**
+     * Ensures that isClass() throws an exception if the given argument
+     * is not a string.
+     */
     public function testIsClassThrowsExceptionIfNoStringIsProvided()
     {
         
     }
     
+    /**
+     * Ensures that isClass() returns false if the given string
+     * is not even a type name.
+     */
     public function testIsClassReturnsFalseIfGivenValueIsNoType()
     {
         
     }
     
-    public function testIsClassReturnsFalseIfGivenValueIsAnInterface()
+    /**
+     * Ensures that isClass() returns false if the given value is an
+     * interface name.
+     */
+    public function testIsClassReturnsFalseIfGivenValueIsAnInterfaceName()
     {
         
     }
     
+    /**
+     * Ensures that isClass() returns true if a class name is provided.
+     */
     public function testIsClassReturnsTrueIfGivenValueIsClassName()
     {
         
     }
     
+    /**
+     * Ensures that isInterface() throws an exception if the given argument
+     * is not a string.
+     */
     public function testIsInterfaceThrowsExceptionIfNoStringIsProvided()
     {
     
     }
     
+    /**
+     * Ensures that isInterface() returns false if the given string
+     * is not even a type name.
+     */
     public function testIsInterfaceReturnsFalseIfGivenValueIsNoType()
     {
     
     }
     
+    /**
+     * Ensures that isInterface() returns false if the given value is a
+     * class name.
+     */
     public function testIsInterfaceReturnsFalseIfGivenValueIsClassName()
     {
     
     }
     
-    public function testIsClassReturnsTrueIfGivenValueIsAnInterfaceName()
+    /**
+     * Ensures that isInterface() returns true if an interface name is provided.
+     */
+    public function testIsInterfaceReturnsTrueIfGivenValueIsAnInterfaceName()
     {
     
     }
     
+    /**
+     * Ensures that isType() throws an exception if the given argument
+     * is not a string.
+     */
     public function testIsTypeThrowsExceptionIfGivenValueIsNoString()
     {
         
     }
     
+    /**
+     * Ensures that isType() returns false if the given string
+     * is not a type name.
+     */
     public function testIsTypeReturnsFalseIfGivenValueIsNoTypeName()
     {
         
     }
     
+    /**
+     * Checks if isType() accepts an interface name.
+     */
     public function testIsTypeReturnsTrueIfGivenValueIsAnInterfaceName()
     {
         
     }
     
+    /**
+     * Checks if isType() accepts a class name.
+     */
     public function testIsTypeReturnsTrueIfGivenValueIsClassName()
     {
         
     }
     
+    /**
+     * Ensures that is() throws an exception if the provided constraint
+     * is not a type name.
+     */
     public function testIsThrowsExceptionIfProvidedConstraintIsNoType()
     {
         
     }
     
+    /**
+     * Ensures that is() throws an exception if a constraint list is provided and
+     * at least one of the constraints is not a type name.
+     */
     public function testIsThrowsExceptionIfAtLeastOneOfTheProvidedConstraintsIsNoType()
     {
         
     }
     
+    /**
+     * Ensures that is() throws an exception if the provided name
+     * is not a string.
+     */
     public function testIsThrowsExceptionIfProvidedValueIsNoString()
     {
         
     }
     
+    /**
+     * Ensures that is() returns an boolean if the given constraint
+     * is a type name.
+     */
     public function testIsReturnsBooleanIfProvidedConstraintIsType()
     {
         
     }
     
+    /**
+     * Ensures that is() returns a boolean if the given constraint list contains
+     * only valid type names.
+     */
     public function testIsReturnsBooleanIfConstraintListContainsOnlyValidTypes()
     {
         
     }
     
+    /**
+     * Ensures that is() returns true if the given list of constraints
+     * is empty.
+     */
+    public function testIsReturnsTrueIfConstraintListIsEmpty()
+    {
+        
+    }
+    
+    /**
+     * Ensures that is() returns false if the given value does not fulfill
+     * the type constraint.
+     */
     public function testIsReturnsFalseIfValueDoesNotFulfillTypeConstraint()
     {
         
     }
     
+    /**
+     * Ensures that is() returns false if the given value does not fulfill
+     * all constraints in the given type list.
+     */
     public function testIsReturnsFalseIfValueDoesNotFulfillAllGivenConstraints()
     {
         
     }
     
-    public function testIsReturnsTrueIfValueFulfillsTypeConstraint()
-    {
-        
-    }
-    
+    /**
+     * Ensures that is() returns true if the value fulfills all type constraints
+     * in the provided constraint list.
+     */
     public function testIsReturnsTrueIfValueFulfillsAllGivenTypeConstraints()
     {
         
     }
     
-    public function testIsReturnsTrueIfValueIsExactlyOfRequestedType()
+    /**
+     * Ensures that is() returns true if the given class name equals
+     * the given constraint exactly.
+     */
+    public function testIsReturnsTrueIfClassNameIsExactlyOfRequestedType()
     {
         
     }
     
+    /**
+     * Ensures that is() returns true if the given interface name equals
+     * the given constraint exactly.
+     */
+    public function testIsReturnsTrueIfInterfaceNameIsExactlyOfRequestedType()
+    {
+    
+    }
+    
+    /**
+     * Ensures that is() returns true if the given class name is a
+     * subclass of the requested type.
+     */
     public function testIsReturnsTrueIfValueIsSubclassOfRequestedType()
     {
     
     }
     
+    /**
+     * Ensures that is() returns true if the given class implements the
+     * requested interface type.
+     */
     public function testIsReturnsTrueIfValueImplementsRequestedInterfaceType()
     {
         
     }
     
+    /**
+     * Ensures that is() returns true if an interface is provided which extends
+     * the requested interface type.
+     */
     public function testIsReturnsTrueIfValueIsAnInterfaceThatExtendsTheRequestedInterfaceType()
     {
         
