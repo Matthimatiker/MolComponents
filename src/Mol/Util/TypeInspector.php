f776e1f5ebb74a7662e9568a1ac526d8573cf98b
Matthias Molitor <matthias@matthimatiker.de>
2012-11-26 22:12:12 +0100

    implemented type inspector (#34)

--- src/Mol/Util/TypeInspector.php
+++ src/Mol/Util/TypeInspector.php
@@ -51,7 +51,13 @@ class Mol_Util_TypeInspector
      */
     public function is($name, $typeOrListOfTypes)
     {
-    
+        $this->assertString($name);
+        $listOfTypes = is_array($typeOrListOfTypes) ? $typeOrListOfTypes : array($typeOrListOfTypes);
+        $this->assertContainsOnlyTypes($listOfTypes);
+        if (!$this->isType($name)) {
+            return false;
+        }
+        return $this->fulfillsConstraints(new ReflectionClass($name), $listOfTypes);
     }
     
     /**
@@ -65,7 +71,7 @@ class Mol_Util_TypeInspector
      */
     public function isType($name)
     {
-        
+        return $this->isClass($name) || $this->isInterface($name);
     }
     
     /**
@@ -77,7 +83,8 @@ class Mol_Util_TypeInspector
      */
     public function isClass($name)
     {
-        
+        $this->assertString($name);
+        return class_exists($name, true);
     }
     
     /**
@@ -89,7 +96,85 @@ class Mol_Util_TypeInspector
      */
     public function isInterface($name)
     {
-        
+        $this->assertString($name);
+        return interface_exists($name, true);
+    }
+    
+    /**
+     * Checks if the provided type fulfills all given type constraints.
+     *
+     * @param ReflectionClass $type
+     * @param array(string) $constraints The type constraints.
+     * @return boolean True if all constraints are fulfilled, false otherwise.
+     */
+    protected function fulfillsConstraints(ReflectionClass $type, array $constraints)
+    {
+        foreach ($constraints as $constraint) {
+            /* @var string $constraint */
+            if (!$this->fulfillsConstraint($type, $constraint)) {
+                return false;
+            }
+        }
+        // Type requirements fulfilled.
+        return true;
+    }
+    
+    /**
+     * Checks if the provided type fulfills the given type constraint.
+     *
+     * @param ReflectionClass $type
+     * @param string $constraint The type constraint.
+     * @return boolean True if the constraint is fulfilled, false otherwise.
+     */
+    protected function fulfillsConstraint(ReflectionClass $type, $constraint)
+    {
+        if ($this->isInterface($constraint) && $type->implementsInterface($constraint)) {
+            // Type implements the required interface.
+            return true;
+        }
+        if ($type->isSubclassOf($constraint)) {
+            // Type is a subclass of the required type.
+            return true;
+        }
+        if ($type->getName() === $constraint) {
+            // Type equals required type.
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Asserts that the given value is a string.
+     *
+     * @param mixed $value
+     * @throws InvalidArgumentException If $value is not a string.
+     */
+    protected function assertString($value)
+    {
+        if (is_string($value)) {
+            return;
+        }
+        $format  = 'String expected, bt received: %s';
+        $message = sprintf($format, Mol_Util_Stringifier::stringify($value));
+        throw new InvalidArgumentException($message);
+    }
+    
+    /**
+     * Asserts that the given list contains only type names.
+     *
+     * @param array(string) $listOfTypes
+     * @throws InvalidArgumentException If one of the items is not a type.
+     */
+    protected function assertContainsOnlyTypes(array $listOfTypes)
+    {
+        $validTypes = array_filter($listOfTypes, array($this, 'isType'));
+        if (count($validTypes) === count($listOfTypes)) {
+            // All types in the given list are valid.
+            return;
+        }
+        $invalidTypes = array_diff($listOfTypes, $validTypes);
+        $message      = 'The following types are not valid: ' . Mol_Util_Stringifier::stringify($invalidTypes);
+        throw new InvalidArgumentException($message);
     }
     
 }
