03a8450e15b57074a1314230d1baab04874514aa
Matthias Molitor <matthias@matthimatiker.de>
2012-12-15 15:07:09 +0100

    added test case stub

---
+++ tests/Mol/Form/Element/CaptchaTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Form_Element_CaptchaTest
+ *
+ * @category PHP
+ * @package Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 15.12.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the captcha element.
+ *
+ * @category PHP
+ * @package Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 15.12.2012
+ */
+class Mol_Form_Element_CaptchaTest extends PHPUnit_Framework_TestCase
+{
+    
+}
