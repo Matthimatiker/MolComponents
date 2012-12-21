e551719e5d8d34e9e1ecdb1d07edaf921376136b
Matthias Molitor <matthias@matthimatiker.de>
2012-12-21 22:53:40 +0100

    added stubs for validator classes

---
+++ src/Mol/Validate/Form/Relation/Contains.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Mol_Validate_Form_Relation_Contains
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 21.12.2012
+ */
+
+/**
+ * Validator which ensures that a value contains another value.
+ *
+ * Can be used in combination with Mol_Validate_Form_ElementRelation.
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 21.12.2012
+ */
+class Mol_Validate_Form_Relation_Contains extends Zend_Validate_Abstract
+{
+    
+}
