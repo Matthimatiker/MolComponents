ac0a073945b61a26c2660f8db58581996538385a
Matthias Molitor <matthias@matthimatiker.de>
2012-12-21 22:57:09 +0100

    added test case stubs

---
+++ tests/Mol/Validate/Form/Relation/NotContainsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Validate_Form_Relation_NotContainsTest
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 21.12.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the NotContains relation validator.
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 21.12.2012
+ */
+class Mol_Validate_Form_Relation_NotContainsTest extends PHPUnit_Framework_TestCase
+{
+    
+}
