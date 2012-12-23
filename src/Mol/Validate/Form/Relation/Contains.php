6958d271aebc576b4b2bdd9c25a5dcb7afc03c21
Matthias Molitor <matthias@matthimatiker.de>
2012-12-23 22:18:21 +0100

    added method stub and failure message template

--- src/Mol/Validate/Form/Relation/Contains.php
+++ src/Mol/Validate/Form/Relation/Contains.php
@@ -28,4 +28,33 @@
 class Mol_Validate_Form_Relation_Contains extends Zend_Validate_Abstract
 {
     
+    /**
+     * Identifier for failure message if value does not
+     * contain the compared value.
+     *
+     * @var string
+     */
+    const NOT_CONTAINS = 'relationContainsValueNotContains';
+    
+    /**
+     * Failure message templates.
+     *
+     * @var array(string=>string)
+     */
+    protected $_messageTemplates = array(
+        self::NOT_CONTAINS => "Input must contain '%compareLabel%'"
+    );
+    
+    /**
+     * Checks if $value contains $other.
+     *
+     * @param string $value
+     * @param string $other
+     * @return boolean True if $value contains $other, false otherwise.
+     */
+    public function isValid($value, $other = null)
+    {
+        
+    }
+    
 }
