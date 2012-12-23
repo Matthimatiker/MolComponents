c000aba5c9bf77d08c5bda85e9e9dbea5e79a250
Matthias Molitor <matthias@matthimatiker.de>
2012-12-23 23:00:55 +0100

    added test case stub

---
+++ tests/Mol/Test/Assertions/HttpResponseTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Mol_Test_Assertions_HttpResponseTest
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 23.12.2012
+ */
+
+/**
+ * Tests the HttpResponse assertions.
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 23.12.2012
+ */
+class Mol_Test_Assertions_HttpResponseTest extends PHPUnit_Framework_TestCase
+{
+    
+}
