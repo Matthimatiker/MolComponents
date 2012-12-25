cbe9c4bcbf06e418e439b7e9e5188ebd8edd447c
Matthias Molitor <matthias@matthimatiker.de>
2012-12-25 13:24:36 +0100

    added test case stub

---
+++ tests/Mol/Test/WebControllerTestCaseTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Test_WebControllerTestCaseTest
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 25.12.2012
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the functionality of the WebControllerTestCase.
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2012 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 25.12.2012
+ */
+class Mol_Test_WebControllerTestCaseTest extends PHPUnit_Framework_TestCase
+{
+    
+}
