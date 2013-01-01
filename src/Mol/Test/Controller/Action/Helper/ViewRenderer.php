a25a1ae101305a43baffe73142aeabe9aba20e61
Matthias Molitor <matthias@matthimatiker.de>
2013-01-02 00:07:46 +0100

    added view renderer stub

---
+++ src/Mol/Test/Controller/Action/Helper/ViewRenderer.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Test_Controller_Action_Helper_ViewRenderer
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 02.01.2013
+ */
+
+/**
+ * A ViewRenderer that avoids global dependencies and is used for testing.
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 02.01.2013
+ */
+class Mol_Test_Controller_Action_Helper_ViewRenderer extends Zend_Controller_Action_Helper_ViewRenderer
+{
+    
+}
