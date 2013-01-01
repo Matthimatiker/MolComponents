18a1f4f03e2818aea7b37503912aee9c16a36830
Matthias Molitor <matthias@matthimatiker.de>
2013-01-01 18:13:44 +0100

    added a new test case that contains internal tests

---
+++ tests/Mol/Test/WebControllerTestCaseTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Mol_Test_WebControllerTestCaseTest
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/*
+ * @since 01.01.2013
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Extends the Mol_Test_WebControllerTestCase and checks its functionality
+ * from an internal popint of view.
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/*
+ * @since 01.01.2013
+ */
+class Mol_Test_WebControllerTestCaseTest extends Mol_Test_WebControllerTestCase
+{
+    
+}
