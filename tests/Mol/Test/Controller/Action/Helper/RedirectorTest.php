6efb686617e2fb28ccbf0718e36088a7e5fff790
Matthias Molitor <matthias@matthimatiker.de>
2013-01-14 23:34:08 +0100

    added test case stub

---
+++ tests/Mol/Test/Controller/Action/Helper/RedirectorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Test_Controller_Action_Helper_RedirectorTest
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 14.01.2013
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the Redirector action helper that is used in controller tests.
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 14.01.2013
+ */
+class Mol_Test_Controller_Action_Helper_RedirectorTest extends PHPUnit_Framework_TestCase
+{
+    
+}
