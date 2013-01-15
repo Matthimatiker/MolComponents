30dc57457dd1028823422df99040ba435c5cfebc
Matthias Molitor <matthias@matthimatiker.de>
2013-01-15 23:53:28 +0100

    added exception type for testing classes

---
+++ src/Mol/Test/Exception.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Mol_Test_Exception
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 15.01.2013
+ */
+
+/**
+ * Exception that is used by the testing helper classes, for example to
+ * indicate restrictions.
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 15.01.2013
+ */
+class Mol_Test_Exception extends Zend_Exception
+{
+    
+}
