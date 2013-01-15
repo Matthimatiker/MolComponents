e6610dfe3fe0f6339a95eea2ad4f1506031ec939
Matthias Molitor <matthias@matthimatiker.de>
2013-01-15 19:58:44 +0100

    added comments

--- tests/Mol/Test/Controller/Action/Helper/RedirectorTest.php
+++ tests/Mol/Test/Controller/Action/Helper/RedirectorTest.php
@@ -33,51 +33,89 @@ require_once(dirname(__FILE__) . '/bootstrap.php');
 class Mol_Test_Controller_Action_Helper_RedirectorTest extends PHPUnit_Framework_TestCase
 {
     
+    /**
+     * Checks if getName() returns the expected value.
+     */
     public function testGetNameReturnsCorrectValue()
     {
         
     }
     
+    /**
+     * Checks if calling exit() is disabled per default, as it
+     * would stop the whole test run.
+     */
     public function testExitIsDisabledPerDefault()
     {
         
     }
     
+    /**
+     * Ensures that it is not possible to enable calls to exit().
+     */
     public function testExitCannotBeEnabled()
     {
         
     }
     
+    /**
+     * Checks if setGotoSimple() uses the controller name from the request
+     * if it is omitted.
+     */
     public function testSetGotoSimpleUsesControllerFromRequestIfNotProvided()
     {
         
     }
     
+    /**
+     * Checks if setGotoSimple() uses the module name from the request
+     * if it is omitted.
+     */
     public function testSetGotoSimpleUsesModuleFromRequestIfNotProvided()
     {
     
     }
     
+    /**
+     * Checks if setGotoSimple() generates a simple url.
+     */
     public function testSetGotSimpleGeneratesSimpleUrl()
     {
         
     }
     
+    /**
+     * Ensures that setGotoSimple() orders the given parameters by key.
+     *
+     * This ensures that the generated url does not depend on the order
+     * in which the parameters are passed.
+     */
     public function testSetGotoSimpleOrdersParametersInUrlByKey()
     {
         
     }
     
+    /**
+     * Checks if setGotoRoute() generates a simple url.
+     */
     public function testSetGotoRouteGeneratesSimpleUrl()
     {
         
     }
     
+    /**
+     * Ensures that access to the front controller is rejected as it
+     * iis a global dependency.
+     */
     public function testHelperDoesNotAllowAccessToFrontController()
     {
         
     }
     
+    /**
+     * Ensures that it is not possible to call redirectAndExit() as it
+     * would terminate the whole test run.
+     */
     public function testHelperDoesNotAllowCallsToRedirectAndExit()
     {
         
