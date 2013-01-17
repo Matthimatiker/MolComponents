b0cefeca5f9949c1b68aa535d6be149e3bcd8d45
Matthias Molitor <matthias@matthimatiker.de>
2013-01-17 21:42:04 +0100

    added test case stub for view renderer

---
+++ tests/Mol/Test/Controller/Action/Helper/ViewRendererTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Test_Controller_Action_Helper_ViewRenderereTest
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/*
+ * @since 17.01.2013
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the view renderer which is used in controller tests.
+ *
+ * @category PHP
+ * @package Mol_Test
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/*
+ * @since 17.01.2013
+ */
+class Mol_Test_Controller_Action_Helper_ViewRenderereTest extends PHPUnit_Framework_TestCase
+{
+    
+}
