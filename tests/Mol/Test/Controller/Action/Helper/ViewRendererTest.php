10d86de7cd898d6649bb7b647b33e9760ba38e6f
Matthias Molitor <matthias@matthimatiker.de>
2013-01-17 21:54:28 +0100

    added test stub

--- tests/Mol/Test/Controller/Action/Helper/ViewRendererTest.php
+++ tests/Mol/Test/Controller/Action/Helper/ViewRendererTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Mol_Test_Controller_Action_Helper_ViewRenderereTest
+ * Mol_Test_Controller_Action_Helper_ViewRendererTest
  *
  * @category PHP
  * @package Mol_Test
@@ -30,7 +30,41 @@ require_once(dirname(__FILE__) . '/bootstrap.php');
  * @link https://github.com/Matthimatiker/*
  * @since 17.01.2013
  */
-class Mol_Test_Controller_Action_Helper_ViewRenderereTest extends PHPUnit_Framework_TestCase
+class Mol_Test_Controller_Action_Helper_ViewRendererTest extends PHPUnit_Framework_TestCase
 {
+    
+    /**
+     * System under test.
+     *
+     * @var Mol_Test_Controller_Action_Helper_ViewRendererer
+     */
+    protected $viewRenderer = null;
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->viewRenderer = new Mol_Test_Controller_Action_Helper_ViewRenderer();
+    }
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        $this->viewRenderer = null;
+        parent::tearDown();
+    }
+    
+    /**
+     * Ensures that access to the front controller is restricted as it
+     * is a global dependency.
+     */
+    public function testHelperDoesNotAllowAccessToFrontController()
+    {
+        
+    }
     
 }
