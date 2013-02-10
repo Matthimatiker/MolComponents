9bfce6fcb460a77e2fa6c699a07f5d5678a514e9
Matthias Molitor <matthias@matthimatiker.de>
2013-02-10 21:42:03 +0100

    added class stub for abstract form plugin

---
+++ src/Mol/Form/Factory/Plugin/AbstractPlugin.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Form_Factory_Plugin_AbstractPlugin
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 10.02.2013
+ */
+
+/**
+ * Optional base class for form factory plugins.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 10.02.2013
+ */
+abstract class Mol_Form_Factory_Plugin_AbstractPlugin
+{
+    
+}
