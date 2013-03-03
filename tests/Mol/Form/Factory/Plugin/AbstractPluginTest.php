6a34c0d37797f42260878f2e62bcf2629b898398
Matthias Molitor <matthias@matthimatiker.de>
2013-03-04 00:57:26 +0100

    added test case stub

---
+++ tests/Mol/Form/Factory/Plugin/AbstractPluginTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Form_Factory_AbstractPluginTest
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 04.03.2013
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the abstract base class for form factory plugins.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 04.03.2013
+ */
+class Mol_Form_Factory_AbstractPluginTest extends PHPUnit_Framework_TestCase
+{
+    
+}
