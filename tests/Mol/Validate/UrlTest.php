c4235b1051e78004213c49eb76f9257bfedc40b6
Matthias Molitor <matthias@matthimatiker.de>
2013-03-11 20:53:34 +0100

    added test case stub

---
+++ tests/Mol/Validate/UrlTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Validate_UrlTest
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 11.03.2013
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the URL validator.
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 11.03.2013
+ */
+class Mol_Validate_UrlTest extends PHPUnit_Framework_TestCase
+{
+    
+}
