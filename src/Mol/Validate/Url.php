fd395ad356cf8d419459300dc27797cd89c786e9
Matthias Molitor <matthias@matthimatiker.de>
2013-03-11 20:50:41 +0100

    added validator stub

---
+++ src/Mol/Validate/Url.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Mol_Validate_Url
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 11.03.2013
+ */
+
+/**
+ * Validator that checks URLs.
+ *
+ * @category PHP
+ * @package Mol_Validate
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 11.03.2013
+ */
+class Mol_Validate_Url extends Zend_Validate_Abstract
+{
+    
+    /**
+     * Checks if $value is an absolute URL.
+     *
+     * @param mixed $value
+     * @return boolean
+     */
+    public function isValid($value)
+    {
+        
+    }
+    
+}
