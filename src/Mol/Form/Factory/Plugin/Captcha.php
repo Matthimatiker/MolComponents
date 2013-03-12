3a74a628d7828cee81f3210da4ff2eeb42304ca7
Matthias Molitor <matthias@matthimatiker.de>
2013-03-12 23:59:58 +0100

    added Captcha form plugin stub

---
+++ src/Mol/Form/Factory/Plugin/Captcha.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Form_Factory_Plugin_Captcha
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 12.03.2013
+ */
+
+/**
+ * Plugin that is able to add captchas to forms.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 12.03.2013
+ */
+class Mol_Form_Factory_Plugin_Captcha extends Mol_Form_Factory_Plugin_AbstractPlugin
+{
+    
+}
