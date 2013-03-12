60cf3054ac9e6ea53c021692bb19fa286735de70
Matthias Molitor <matthias@matthimatiker.de>
2013-03-13 00:00:07 +0100

    added test case stub

---
+++ tests/Mol/Form/Factory/Plugin/CaptchaTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Form_Factory_Plugin_Captcha
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 12.03.2013
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the Captcha form plugin.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 12.03.2013
+ */
+class Mol_Form_Factory_Plugin_CaptchaTest extends PHPUnit_Framework_TestCase
+{
+    
+}
