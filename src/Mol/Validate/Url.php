f88d723eb30bbe9f09aba674402c93278c69ff05
Matthias Molitor <matthias@matthimatiker.de>
2013-03-12 21:30:02 +0100

    implemented URL validator (#58)

--- src/Mol/Validate/Url.php
+++ src/Mol/Validate/Url.php
@@ -27,6 +27,32 @@ class Mol_Validate_Url extends Zend_Validate_Abstract
 {
     
     /**
+     * Key for failure message which indicates that a non-string
+     * value was provided.
+     *
+     * @var string
+     */
+    const FAILURE_INVALID = 'urlInvalid';
+    
+    /**
+     * Key for failure message which indicates that the validated
+     * value is no absolute url.
+     *
+     * @var string
+     */
+    const FAILURE_NO_URL = 'urlNoUrl';
+    
+    /**
+     * Default failure messages.
+     *
+     * @var array(string)
+     */
+    protected $_messageTemplates = array(
+        self::FAILURE_INVALID => "Invalid type given. String expected, but received %value%",
+        self::FAILURE_NO_URL  => "'%value%' is no absolute URL"
+    );
+    
+    /**
      * Checks if $value is an absolute URL.
      *
      * @param mixed $value
@@ -34,7 +60,16 @@ class Mol_Validate_Url extends Zend_Validate_Abstract
      */
     public function isValid($value)
     {
-        
+        $this->_setValue($value);
+        if (!is_string($value)) {
+            $this->_error(self::FAILURE_INVALID, Mol_Util_Stringifier::stringify($value));
+            return false;
+        }
+        if (!Zend_Uri_Http::check($value)) {
+            $this->_error(self::FAILURE_NO_URL);
+            return false;
+        }
+        return true;
     }
     
 }
