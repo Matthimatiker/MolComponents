97eff3b0adc35c735cabacf08a475c08140288a1
Matthias Molitor <matthias@matthimatiker.de>
2013-03-24 15:54:46 +0100

    added element class stub

---
+++ src/Mol/Form/Element/Url.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Form_Element_Url
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 24.03.2013
+ */
+
+/**
+ * Form element that accepts a URL as input.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 24.03.2013
+ */
+class Mol_Form_Element_Url extends Zend_Form_Element_Text
+{
+    
+}
