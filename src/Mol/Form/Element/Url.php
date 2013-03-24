daf692cf9ea65b3107644345bfba3f0d71bfbb6e
Matthias Molitor <matthias@matthimatiker.de>
2013-03-24 21:07:34 +0100

    added method stubs

--- src/Mol/Form/Element/Url.php
+++ src/Mol/Form/Element/Url.php
@@ -26,4 +26,60 @@
 class Mol_Form_Element_Url extends Zend_Form_Element_Text
 {
     
+    /**
+     * System under test.
+     *
+     * @var Mol_Form_Element_Url
+     */
+    protected $element = null;
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->element = new Mol_Form_Element_Url('url');
+    }
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        $this->element = null;
+        parent::tearDown();
+    }
+    
+    /**
+     * Sets hostnames that are allowed in the url.
+     *
+     * @param array(string) $hostnames
+     * @return Mol_Form_Element_Url Provides a fluent interface.
+     */
+    public function setAllowedHostnames(array $hostnames)
+    {
+        
+    }
+    
+    /**
+     * Returns a list of allowed hostnames.
+     *
+     * @return array(string)
+     */
+    public function getAllowedHostnames()
+    {
+        
+    }
+    
+    /**
+     * Checks if hostname restrictions are active.
+     *
+     * @return boolean True if the hostname is restricted, false otherwise.
+     */
+    public function hasHostnameRestrictions()
+    {
+        
+    }
+    
 }
