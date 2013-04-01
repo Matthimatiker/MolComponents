bbb1523fae592bf99c1457b92d4e6f25bb2c1cf0
Matthias Molitor <matthias@matthimatiker.de>
2013-04-01 13:35:42 +0200

    added class stub

---
+++ src/Mol/Form/Factory/Plugin/UniqueId.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Mol_Form_Factory_Plugin_UniqueId
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.04.2013
+ */
+
+/**
+ * Plugins that sets unique IDs for each form elements.
+ *
+ * Assigning unique IDs avoids clashes if the same form is rendered
+ * twice on the same page or if different forms ccontain elements
+ * with the same name.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.04.2013
+ */
+class Mol_Form_Factory_Plugin_UniqueId extends Mol_Form_Factory_Plugin_AbstractPlugin
+{
+    
+}
