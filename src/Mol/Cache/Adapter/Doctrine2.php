89e52a26d39662fe3ff4d3a107b08aa4531d95ae
Matthias Molitor <matthias@matthimatiker.de>
2013-04-01 23:56:01 +0200

    added cache adapter and test case class stubs

---
+++ src/Mol/Cache/Adapter/Doctrine2.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Cache_Adapter_Doctrine2
+ *
+ * @category PHP
+ * @package Mol_Cache
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.04.2013
+ */
+
+/**
+ * Adapter that allows the usage of Doctrine 2 caches in Zend applications.
+ *
+ * @category PHP
+ * @package Mol_Cache
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.04.2013
+ */
+class Mol_Cache_Adapter_Doctrine2
+{
+    
+}
---
+++ tests/Mol/Cache/Adapter/Doctrine2Test.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Cache_Adapter_Doctrine2
+ *
+ * @category PHP
+ * @package Mol_Cache
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.04.2013
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Tests the Doctrine 2 cache adapter.
+ *
+ * @category PHP
+ * @package Mol_Cache
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 01.04.2013
+ */
+class Mol_Cache_Adapter_Doctrine2Test extends PHPUnit_Framework_TestCase
+{
+    
+}
