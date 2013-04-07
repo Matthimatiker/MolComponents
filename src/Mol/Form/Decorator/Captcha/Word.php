fecc23a787e43882b72fd18026f17e8ac1df1523
Matthias Molitor <matthias@matthimatiker.de>
2013-04-07 18:52:53 +0200

    added decorator and test case stub

---
+++ src/Mol/Form/Decorator/Captcha/Word.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Form_Decorator_Captcha_Word
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 07.04.2013
+ */
+
+/**
+ * A captcha decorator that fixes problems regarding ID rendering.
+ *
+ * @category PHP
+ * @package Mol_Form
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 07.04.2013
+ */
+class Mol_Form_Decorator_Captcha_Word extends Zend_Form_Decorator_Captcha_Word
+{
+    
+}
