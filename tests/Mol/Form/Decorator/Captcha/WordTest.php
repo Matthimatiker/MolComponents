a91dc4ff641493d9d0f191e3658da3203c093bbc
Matthias Molitor <matthias@matthimatiker.de>
2013-04-08 22:19:24 +0200

    added comments

--- tests/Mol/Form/Decorator/Captcha/WordTest.php
+++ tests/Mol/Form/Decorator/Captcha/WordTest.php
@@ -51,21 +51,36 @@ class Mol_Form_Decorator_Captcha_WordTest extends PHPUnit_Framework_TestCase
         
     }
     
+    /**
+     * Ensures that the decorator does not assign the same ID to the hidden
+     * and the text input field.
+     */
     public function testDecoratorDoesNotAssignSameIdToHiddenAndTextField()
     {
         
     }
     
+    /**
+     * Checks if the ID with suffix "-input" is assigned to the text field.
+     */
     public function testDecoratorAssignsIdWithInputSuffixToTextField()
     {
         
     }
     
+    /**
+     * Ensures that the decorator does not break the provided content string if it contains
+     * the element ID.
+     */
     public function testDecoratorDoesNotDestroyInitialContentIfItContainsIdAsString()
     {
         
     }
     
+    /**
+     * Checks if the decorator works with an element that does not define an ID
+     * explicitly.
+     */
     public function testDecoratorWorksIfElementDoesNotProvideIdExplicitly()
     {
         
