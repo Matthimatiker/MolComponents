58af43f011d82a0b78c1ddbd5fd5d3eb1c24c88f
Matthias Molitor <matthias@matthimatiker.de>
2013-04-11 22:52:10 +0200

    implemented cache and added documentation

--- src/Mol/Cache/Backend/Memory.php
+++ src/Mol/Cache/Backend/Memory.php
@@ -12,11 +12,34 @@
  * @since 11.04.2013
  */
 
+use Doctrine\Common\Cache\ArrayCache;
+
 /**
  * Cache backend that holds its data in the memory.
  *
  * The cache data is lost when the PHP script terminates.
  * Different cache objects do not share their data.
+ *
+ * # Requirements #
+ *
+ * Internally this cache uses a Doctrine 2 cache instance, therefore
+ * the package "doctrine/cache" is needed for this component to work.
+ *
+ * # Usage #
+ *
+ * No arguments are needed to create memory cache instances:
+ *
+ *     $backend = new Mol_Cache_Backend_Memory();
+ *
+ * Afterwards backend can be used by a cache instance:
+ *
+ *     $cache = new Zend_Cache_Core();
+ *     $cache->setBackend($backend);
+ *
+ * It is also possible to use the factory() method of Zend_Cache_Core
+ * to create a cache with memory backend:
+ *
+ *     $cache = Zend_Cache_Core::factory('Core', 'Mol_Cache_Backend_Memory', array(), array(), false, true, true);
  *
  * @category PHP
  * @package Mol_Cache
@@ -26,7 +49,15 @@
  * @link https://github.com/Matthimatiker/MolComponents
  * @since 11.04.2013
  */
-class Mol_Cache_Backend_Memory
+class Mol_Cache_Backend_Memory extends Mol_Cache_Adapter_Doctrine2ToZend
 {
+    
+    /**
+     * Creates a cache instance.
+     */
+    public function __construct()
+    {
+        parent::__construct(new ArrayCache());
+    }
     
 }
