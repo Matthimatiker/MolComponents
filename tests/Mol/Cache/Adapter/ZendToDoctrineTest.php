d7e5ad5f720c65ba69f03a152efd8bb5ae254275
Matthias Molitor <matthias@matthimatiker.de>
2013-04-13 17:29:07 +0200

    added test case stub

---
+++ tests/Mol/Cache/Adapter/ZendToDoctrineTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mol_Cache_Adapter_ZendToDoctrine
+ *
+ * @category PHP
+ * @package Mol_Cache
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 13.04.2013
+ */
+
+/**
+ * Initializes the test environment.
+ */
+require_once(dirname(__FILE__) . '/bootstrap.php');
+
+/**
+ * Mol_Cache_Adapter_ZendToDoctrine
+ *
+ * @category PHP
+ * @package Mol_Cache
+ * @subpackage Tests
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 13.04.2013
+ */
+class Mol_Cache_Adapter_ZendToDoctrineTest extends PHPUnit_Framework_TestCase
+{
+    
+}
