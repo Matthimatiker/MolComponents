10fdc60390c71647da1fb31966623d77bb4d1625
Matthias Molitor <matthias@matthimatiker.de>
2013-04-13 17:27:26 +0200

    added adapter stub

---
+++ src/Mol/Cache/Adapter/ZendToDoctrine.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Mol_Cache_Adapter_ZendToDoctrine
+ *
+ * @category PHP
+ * @package Mol_Cache
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 13.04.2013
+ */
+
+/**
+ * Adapter that allows the usage of Zend caches with Doctrine 2.
+ *
+ * @category PHP
+ * @package Mol_Cache
+ * @author Matthias Molitor <matthias@matthimatiker.de>
+ * @copyright 2013 Matthias Molitor
+ * @license http://www.opensource.org/licenses/BSD-3-Clause BSD License
+ * @link https://github.com/Matthimatiker/MolComponents
+ * @since 13.04.2013
+ */
+class Mol_Cache_Adapter_ZendToDoctrine
+{
+    
+}
