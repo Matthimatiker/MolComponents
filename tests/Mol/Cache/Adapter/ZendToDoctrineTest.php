37dbb4d56dfaef8a8e2e51ab0f87d5363fe6e4fb
Matthias Molitor <matthias@matthimatiker.de>
2013-04-13 22:41:48 +0200

    implemented some tests, added more test stubs

--- tests/Mol/Cache/Adapter/ZendToDoctrineTest.php
+++ tests/Mol/Cache/Adapter/ZendToDoctrineTest.php
@@ -34,11 +34,45 @@ class Mol_Cache_Adapter_ZendToDoctrineTest extends PHPUnit_Framework_TestCase
 {
     
     /**
+     * System under test.
+     *
+     * @var Mol_Cache_Adapter_ZendToDoctrine
+     */
+    protected $adapter = null;
+    
+    /**
+     * The cache that is passed to the adapter.
+     *
+     * @var Zend_Cache_Backend_Interface
+     */
+    protected $innerCache = null;
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::setUp()} for details.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->innerCache = new Mol_Cache_Backend_Memory();
+        $this->adapter    = new Mol_Cache_Adapter_ZendToDoctrine($this->innerCache);
+    }
+    
+    /**
+     * See {@link PHPUnit_Framework_TestCase::tearDown()} for details.
+     */
+    protected function tearDown()
+    {
+        $this->adapter    = null;
+        $this->innerCache = null;
+        parent::tearDown();
+    }
+    
+    /**
      * Checks if the adapter implements the Doctrine cache interface.
      */
     public function testImplementsCacheInterface()
     {
-        
+        $this->assertInstanceOf('Doctrine\Common\Cache\Cache', $this->adapter);
     }
     
     /**
@@ -100,6 +134,23 @@ class Mol_Cache_Adapter_ZendToDoctrineTest extends PHPUnit_Framework_TestCase
     }
     
     /**
+     * Ensures that non-string values are converted to strings before the adapter
+     * passes them to the inner cache.
+     */
+    public function testSaveConvertsNonStringValuesToStringBeforePassingThemToInnerCache()
+    {
+        
+    }
+    
+    /**
+     * Checks if fetch() returns a cached non-string item correctly.
+     */
+    public function testFetchReturnsCachedNonStringItemCorrectly()
+    {
+        
+    }
+    
+    /**
      * Checks if delete() removes an item.
      */
     public function testDeleteRemovesItem()
@@ -113,7 +164,7 @@ class Mol_Cache_Adapter_ZendToDoctrineTest extends PHPUnit_Framework_TestCase
      */
     public function testGetStatsReturnsNull()
     {
-        
+        $this->assertNull($this->adapter->getStats());
     }
     
     /**
@@ -121,7 +172,7 @@ class Mol_Cache_Adapter_ZendToDoctrineTest extends PHPUnit_Framework_TestCase
      */
     public function testGetInnerCacheReturnsCacheThatWasPassedToTheConstructor()
     {
-        
+        $this->assertSame($this->innerCache, $this->adapter->getInnerCache());
     }
     
 }
